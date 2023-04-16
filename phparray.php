282983321548e44d962e7230ff178457a9dd6ad8
arnica13 <89857852+arnica13@users.noreply.github.com>
2023-04-16 17:20:39 +0700

    Add files via upload

---
+++ phparray.php
@@ -0,0 +1,16 @@
+<html> 
+    <head>
+        <title> Pemrograman PHP dengan Array </title>
+    </head>
+    <body>
+        <?php
+        //penulisan array dapat dibuat seperti berikut
+        $nama[] = "An-Nisaa";
+        $nama[] = "Respati";
+        $nama[] = "Nurcahyengsi";
+        echo $nama[1] . $nama[2] . $nama[0];
+        echo "<br>";
+        ?>
+    </body>
+</html>
+
---
+++ requestAct.php
@@ -0,0 +1,8 @@
+<?php
+if(isset($_REQUEST['An-Nisaa']){
+    echo "<center>Nama :".$_REQUEST['nama']."</center><br>";
+}
+else{
+    echo "Data tidak tersedia";
+}
+?>
